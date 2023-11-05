75035f12272d543f15290bb375251167a42df26c
reek004 <chatterjeeswarnabha@gmail.com>
2023-11-05 13:25:14 +0530

    Edited the compare function to print non exsisting letters in the list

--- GameMech/gameMain.py
+++ GameMech/gameMain.py
@@ -1,23 +1,25 @@
 import random
 from spellchecker import SpellChecker
 spell = SpellChecker()
-from colorama import Fore
+from colorama import Fore,Back  #Module to import the color
 
-def newWord():
+def newWord():   #Function to generate new Word          
     with open("/home/reek/Desktop/WordleGame/GameMech/wordList.txt") as f:
         mylist = list(f)
     word = random.choice(mylist)
-    genList = creating_list(word)
+    print(word)
+    genList = creating_list(word)  # Calling the creating list
     print(genList)
     return genList 
+    #Returns the list for the generated word
 
 def takeInput():
     userWord = input("")
     if len(userWord) != 5:
-        print("Must be 5 letters")
+        print("Must be 5 letters")   #If length is not 5 call the function again
     else:
         userList = check_word_spelling(userWord)
-        return userList
+        return userList    #return userList
 
 def check_word_spelling(word):
     corrected_word = spell.correction(word)
@@ -29,17 +31,21 @@ def check_word_spelling(word):
         return []  # Return an empty list to indicate an invalid word
 
 def creating_list(word):
-    s = list(word.strip())
+    s = list(word.strip()) #Strip removes the nextline 
     return s
 
-def compare(genList, userList):
+def compare(genList, userList):  #Function for comparing
     if len(genList) != len(userList):
         print("Both lists must have the same length.")
-        return
-
-    for i in range(0, len(genList)):
-        if userList[i] != genList[i]:
-            print(Fore.CYAN + userList[i], end="")
+    else :    
+        for user_item in userList:
+            not_present = True
+            for gen_item in genList:
+                if user_item == gen_item:
+                    not_present = False
+                    break
+            if not_present:
+                print(Fore.CYAN + user_item, end=" ")
 
 gl = newWord()
 ul = takeInput()
