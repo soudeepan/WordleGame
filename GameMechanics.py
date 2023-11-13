68384d1a9fcdf8fca4ce75bce6b085372cde64f7
soudeepan <soudeepanbiswas@gmail.com>
2023-11-13 19:36:46 +0530

    Added the function to change the letter of the labels

--- GameMechanics.py
+++ GameMechanics.py
@@ -11,35 +11,45 @@ def isValid(word):
 
 inputCounter=0
 
-def changeSubheading():
+def change_heading():
+    
     global inputCounter
-
     check_word=inputBox.get()
 
-
     if inputCounter==0:
-        subLabel.config(text="Enter Guess words")
+        subLabel.config(text=check_word + " ! Enter your Guess word")
         inputCounter+=1
     
-    elif inputCounter==6  and isValid(check_word) :
+    elif inputCounter==6 and isValid(check_word) :
         subLabel.config(text="YOU LOSE!")
         enterButton.config(DISABLED)
 
-    elif isValid(check_word) :
-        print(check_word)
+
+    elif isValid(check_word):
         inputCounter+=1
 
     
 # def for updating the labels
 
-def updateLabels(word):
 
-    global wordCount
-
-    for i in range(5):
-        labels[wordCount].config(text=word[i])
+def updateLabels(inputWord):
     
-  
+    inputWord.strip()
+    inputWord = inputWord.upper()
+    for col in range(5):
+        exec(f"label_{inputCounter-1}_{col}.config(text=inputWord[col])")
+
+        
+# def for performing all functions
+
+def allFunctions(inputWord):
+    
+    
+    if inputCounter in range(1,8) and isValid(inputWord):
+        updateLabels(inputWord)
+
+    change_heading()
+
 
 
 # The UI of Wordle Game
@@ -64,7 +74,7 @@ subLabel.pack(pady=5)
 inputBox = Entry(root, font=(my_font,10),borderwidth="3",relief="flat",bg=grey,fg=white)
 inputBox.pack(pady=10)
 
-enterButton = Button(root,font=(my_font, 16),text="Submit", command=changeSubheading,bg=yellow,fg=black)
+enterButton = Button(root,font=(my_font, 16),text="Submit", command=lambda:allFunctions(inputBox.get()),bg=yellow,fg=black)
 enterButton.pack(pady=10)
 
 # The Labels that contains each letter of words
@@ -78,22 +88,16 @@ box_fg = "#FFFFFF"
 box_font = ("Roboto", 20)
 
 x_start = 250  # x-coordinate of labels
-y_start = 250  # y-coordinate of labelsS
-
-labels = []
+y_start = 250  # y-coordinate of labels
 
 for row in range(6):
     ybox = y_start + row * 40
     for col in range(5):
         xbox = x_start + col * 40
         text = " "
-
-        label = Label(root, borderwidth=box_borderwidth, relief=box_relief, bg=box_bg, fg=box_fg,
-                      height=box_height, width=box_width, text=text, font=box_font)
-        label.place(x=xbox, y=ybox)
-
-        labels.append(label)
-
+        
+        exec(f"label_{row}_{col} = Label(root, borderwidth=box_borderwidth, relief=box_relief, bg=box_bg, fg=box_fg,height=box_height, width=box_width, text=text, font=box_font)")
+        exec(f"label_{row}_{col}.place(x=xbox, y=ybox)")
 
 
 root.geometry("700x500")
